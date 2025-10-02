652e8a260c87b0a744ce7977624708bab1c06f73
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2025-10-01 20:54:11 -0500

    Mejora al Ejercicio numero dos y uso del ciclo for

--- Tarea S1.1/src/Ejercicio2.java
+++ Tarea S1.1/src/Ejercicio2.java
@@ -1,51 +1,51 @@
 //Ejercicio 2
 import java.util.Scanner;
 public class Ejercicio2 {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         //Linea de codigo para el Scanner
-        Scanner clientes= new Scanner(System.in);
+        Scanner clientes = new Scanner(System.in);
 
         //Variables
-        System.out.println("========== REGISTRO DE CLIENTES ==========");
         String nombre;
         String cedula;
         String telefono;
         String correo;
 
         //Sistema para el registro de los clientes
-        //Cedula
-        System.out.println("Ingrese su cedula: ");
-        cedula=clientes.nextLine();
-        if(cedula.isEmpty()){ //Usamos la funcion "isEmpty" para validar que el campo no este vacio
-            System.out.println("Error. El campo no puede estar vacio"); //Tomar en cuenta que isEmpty funciona solo con variable tipo String
-            return;
-        }
-        System.out.println("Ingrese sus nombres y apellidos: ");
-        nombre=clientes.nextLine();
-        if(nombre.isEmpty()){
-            System.out.println("Error. El campo no puede estar vacio");
-            return;
-        }
-        System.out.println("Ingrese su numero de telefono: ");
-        telefono=clientes.nextLine();
-        if(telefono.isEmpty()){
-            System.out.println("Error. El campo no puede estar vacio");
-            return;
-        }
-        System.out.println("Ingrese su correo: ");
-        correo=clientes.nextLine();
-        if(correo.isEmpty()){
-            System.out.println("Error. El campo no puede estar vacio");
-            return;
-        }
-
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("========== REGISTRO DE CLIENTE #" + i + " ==========");
+            //Cedula
+            System.out.println("Ingrese su cedula: ");
+            cedula = clientes.nextLine();
+            if (cedula.isEmpty()) { //Usamos la funcion "isEmpty" para validar que el campo no este vacio
+                System.out.println("Error. El campo no puede estar vacio"); //Tomar en cuenta que isEmpty funciona solo con variable tipo String
+                return;
+            }
+            System.out.println("Ingrese sus nombres y apellidos: ");
+            nombre = clientes.nextLine();
+            if (nombre.isEmpty()) {
+                System.out.println("Error. El campo no puede estar vacio");
+                return;
+            }
+            System.out.println("Ingrese su numero de telefono: ");
+            telefono = clientes.nextLine();
+            if (telefono.isEmpty()) {
+                System.out.println("Error. El campo no puede estar vacio");
+                return;
+            }
+            System.out.println("Ingrese su correo: ");
+            correo = clientes.nextLine();
+            if (correo.isEmpty()) {
+                System.out.println("Error. El campo no puede estar vacio");
+                return;
+            }
         //Ficha con los datos del cliente
         System.out.println("======== FICHA DEL CLIENTE ========");
-        System.out.println("Cedula: "+cedula);
-        System.out.println("Nombre:"+nombre);
-        System.out.println("Telefono:"+telefono);
-        System.out.println("Correo:"+correo);
-
-        clientes.close(); //Cerrar el Scanner
+        System.out.println("Cedula: " + cedula);
+        System.out.println("Nombre:" + nombre);
+        System.out.println("Telefono:" + telefono);
+        System.out.println("Correo:" + correo);
     }
+        clientes.close(); //Cerrar el Scanner
+ }
 }
