e1aaf6cdd0ff1b012b568c72e2158eb49d1bca9f
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2025-10-01 19:56:22 -0500

    Implementacion de la validacion para campos vacíos

---
+++ Tarea S1.1/src/Ejercicio2.java
@@ -0,0 +1,37 @@
+//Ejercicio 2
+import java.util.Scanner;
+public class Ejercicio2 {
+    public static void main(String [] args){
+        //Linea de codigo para el Scanner
+        Scanner clientes= new Scanner(System.in);
+
+        //Variables
+        System.out.println("========== REGISTRO DE CLIENTES ==========");
+        String nombre;
+        String cedula;
+        String telefono;
+        String correo;
+
+        //Sistema para el registro de los clientes
+        //Cedula
+        System.out.println("Ingrese su cedula: ");
+        cedula=clientes.nextLine();
+        if(cedula.isEmpty()){ //Usamos la funcion "isEmpty" para validar que el campo no este vacio
+            System.out.println("Error. El campo no puede estar vacio"); //Tomar en cuenta que isEmpty funciona solo con variable tipo String
+            return;
+        }
+        System.out.println("Ingrese sus nombres y apellidos: ");
+        nombre=clientes.nextLine();
+        if(nombre.isEmpty()){
+            System.out.println("Error. El campo no puede estar vacio");
+            return;
+        }
+        System.out.println("Ingrese su numero de telefono: ");
+        telefono=clientes.nextLine();
+        if(telefono.isEmpty()){
+            System.out.println("Error. El campo no puede estar vacio");
+            return;
+        }
+
+    }
+}
