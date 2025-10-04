b692d504b839b7d1af7defeb48ed8bd72703ff8a
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2025-10-04 18:22:56 -0500

    Culminacion del ejercicio 3

---
+++ Tarea S1.2/src/Ejercicio3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //variables
+        int numero;
+        int resultado=0;
+        //Resultados
+        System.out.print("Ingrese el numero(del 1 al 10): ");//Pedir un numero al usuario
+        numero=sc.nextInt();
+        if(numero>=1 && numero<=10) {
+            for (int i = 1; i <= 10; i++) {
+                resultado = numero * i;
+                System.out.println(numero + "*" + i + "=" + resultado);
+            }
+        }else {
+            System.out.println("El numero no es valido");
+        }
+    }
+}
